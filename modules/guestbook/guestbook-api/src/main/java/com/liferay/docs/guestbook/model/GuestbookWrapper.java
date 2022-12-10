52a5b55c387b5c522f6ecd6fe2e02d75d660e5a5
Marcelino Rutea López <marcelino.rutea@civica-soft.com>
2022-12-10 19:18:20 +0100

    second commit, code push

---
+++ modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/model/GuestbookWrapper.java
@@ -0,0 +1,498 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.docs.guestbook.model;
+
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.exportimport.kernel.lar.StagedModelType;
+import com.liferay.portal.kernel.model.ModelWrapper;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * <p>
+ * This class is a wrapper for {@link Guestbook}.
+ * </p>
+ *
+ * @author liferay
+ * @see Guestbook
+ * @generated
+ */
+public class GuestbookWrapper implements Guestbook, ModelWrapper<Guestbook> {
+
+	public GuestbookWrapper(Guestbook guestbook) {
+		_guestbook = guestbook;
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return Guestbook.class;
+	}
+
+	@Override
+	public String getModelClassName() {
+		return Guestbook.class.getName();
+	}
+
+	@Override
+	public Map<String, Object> getModelAttributes() {
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put("uuid", getUuid());
+		attributes.put("guestbookId", getGuestbookId());
+		attributes.put("groupId", getGroupId());
+		attributes.put("companyId", getCompanyId());
+		attributes.put("userId", getUserId());
+		attributes.put("userName", getUserName());
+		attributes.put("createDate", getCreateDate());
+		attributes.put("modifiedDate", getModifiedDate());
+		attributes.put("name", getName());
+
+		return attributes;
+	}
+
+	@Override
+	public void setModelAttributes(Map<String, Object> attributes) {
+		String uuid = (String)attributes.get("uuid");
+
+		if (uuid != null) {
+			setUuid(uuid);
+		}
+
+		Long guestbookId = (Long)attributes.get("guestbookId");
+
+		if (guestbookId != null) {
+			setGuestbookId(guestbookId);
+		}
+
+		Long groupId = (Long)attributes.get("groupId");
+
+		if (groupId != null) {
+			setGroupId(groupId);
+		}
+
+		Long companyId = (Long)attributes.get("companyId");
+
+		if (companyId != null) {
+			setCompanyId(companyId);
+		}
+
+		Long userId = (Long)attributes.get("userId");
+
+		if (userId != null) {
+			setUserId(userId);
+		}
+
+		String userName = (String)attributes.get("userName");
+
+		if (userName != null) {
+			setUserName(userName);
+		}
+
+		Date createDate = (Date)attributes.get("createDate");
+
+		if (createDate != null) {
+			setCreateDate(createDate);
+		}
+
+		Date modifiedDate = (Date)attributes.get("modifiedDate");
+
+		if (modifiedDate != null) {
+			setModifiedDate(modifiedDate);
+		}
+
+		String name = (String)attributes.get("name");
+
+		if (name != null) {
+			setName(name);
+		}
+	}
+
+	@Override
+	public Object clone() {
+		return new GuestbookWrapper((Guestbook)_guestbook.clone());
+	}
+
+	@Override
+	public int compareTo(Guestbook guestbook) {
+		return _guestbook.compareTo(guestbook);
+	}
+
+	/**
+	 * Returns the company ID of this guestbook.
+	 *
+	 * @return the company ID of this guestbook
+	 */
+	@Override
+	public long getCompanyId() {
+		return _guestbook.getCompanyId();
+	}
+
+	/**
+	 * Returns the create date of this guestbook.
+	 *
+	 * @return the create date of this guestbook
+	 */
+	@Override
+	public Date getCreateDate() {
+		return _guestbook.getCreateDate();
+	}
+
+	@Override
+	public ExpandoBridge getExpandoBridge() {
+		return _guestbook.getExpandoBridge();
+	}
+
+	/**
+	 * Returns the group ID of this guestbook.
+	 *
+	 * @return the group ID of this guestbook
+	 */
+	@Override
+	public long getGroupId() {
+		return _guestbook.getGroupId();
+	}
+
+	/**
+	 * Returns the guestbook ID of this guestbook.
+	 *
+	 * @return the guestbook ID of this guestbook
+	 */
+	@Override
+	public long getGuestbookId() {
+		return _guestbook.getGuestbookId();
+	}
+
+	/**
+	 * Returns the modified date of this guestbook.
+	 *
+	 * @return the modified date of this guestbook
+	 */
+	@Override
+	public Date getModifiedDate() {
+		return _guestbook.getModifiedDate();
+	}
+
+	/**
+	 * Returns the name of this guestbook.
+	 *
+	 * @return the name of this guestbook
+	 */
+	@Override
+	public String getName() {
+		return _guestbook.getName();
+	}
+
+	/**
+	 * Returns the primary key of this guestbook.
+	 *
+	 * @return the primary key of this guestbook
+	 */
+	@Override
+	public long getPrimaryKey() {
+		return _guestbook.getPrimaryKey();
+	}
+
+	@Override
+	public Serializable getPrimaryKeyObj() {
+		return _guestbook.getPrimaryKeyObj();
+	}
+
+	/**
+	 * Returns the user ID of this guestbook.
+	 *
+	 * @return the user ID of this guestbook
+	 */
+	@Override
+	public long getUserId() {
+		return _guestbook.getUserId();
+	}
+
+	/**
+	 * Returns the user name of this guestbook.
+	 *
+	 * @return the user name of this guestbook
+	 */
+	@Override
+	public String getUserName() {
+		return _guestbook.getUserName();
+	}
+
+	/**
+	 * Returns the user uuid of this guestbook.
+	 *
+	 * @return the user uuid of this guestbook
+	 */
+	@Override
+	public String getUserUuid() {
+		return _guestbook.getUserUuid();
+	}
+
+	/**
+	 * Returns the uuid of this guestbook.
+	 *
+	 * @return the uuid of this guestbook
+	 */
+	@Override
+	public String getUuid() {
+		return _guestbook.getUuid();
+	}
+
+	@Override
+	public int hashCode() {
+		return _guestbook.hashCode();
+	}
+
+	@Override
+	public boolean isCachedModel() {
+		return _guestbook.isCachedModel();
+	}
+
+	@Override
+	public boolean isEscapedModel() {
+		return _guestbook.isEscapedModel();
+	}
+
+	@Override
+	public boolean isNew() {
+		return _guestbook.isNew();
+	}
+
+	@Override
+	public void persist() {
+		_guestbook.persist();
+	}
+
+	@Override
+	public void setCachedModel(boolean cachedModel) {
+		_guestbook.setCachedModel(cachedModel);
+	}
+
+	/**
+	 * Sets the company ID of this guestbook.
+	 *
+	 * @param companyId the company ID of this guestbook
+	 */
+	@Override
+	public void setCompanyId(long companyId) {
+		_guestbook.setCompanyId(companyId);
+	}
+
+	/**
+	 * Sets the create date of this guestbook.
+	 *
+	 * @param createDate the create date of this guestbook
+	 */
+	@Override
+	public void setCreateDate(Date createDate) {
+		_guestbook.setCreateDate(createDate);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(
+		com.liferay.portal.kernel.model.BaseModel<?> baseModel) {
+
+		_guestbook.setExpandoBridgeAttributes(baseModel);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(ExpandoBridge expandoBridge) {
+		_guestbook.setExpandoBridgeAttributes(expandoBridge);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(ServiceContext serviceContext) {
+		_guestbook.setExpandoBridgeAttributes(serviceContext);
+	}
+
+	/**
+	 * Sets the group ID of this guestbook.
+	 *
+	 * @param groupId the group ID of this guestbook
+	 */
+	@Override
+	public void setGroupId(long groupId) {
+		_guestbook.setGroupId(groupId);
+	}
+
+	/**
+	 * Sets the guestbook ID of this guestbook.
+	 *
+	 * @param guestbookId the guestbook ID of this guestbook
+	 */
+	@Override
+	public void setGuestbookId(long guestbookId) {
+		_guestbook.setGuestbookId(guestbookId);
+	}
+
+	/**
+	 * Sets the modified date of this guestbook.
+	 *
+	 * @param modifiedDate the modified date of this guestbook
+	 */
+	@Override
+	public void setModifiedDate(Date modifiedDate) {
+		_guestbook.setModifiedDate(modifiedDate);
+	}
+
+	/**
+	 * Sets the name of this guestbook.
+	 *
+	 * @param name the name of this guestbook
+	 */
+	@Override
+	public void setName(String name) {
+		_guestbook.setName(name);
+	}
+
+	@Override
+	public void setNew(boolean n) {
+		_guestbook.setNew(n);
+	}
+
+	/**
+	 * Sets the primary key of this guestbook.
+	 *
+	 * @param primaryKey the primary key of this guestbook
+	 */
+	@Override
+	public void setPrimaryKey(long primaryKey) {
+		_guestbook.setPrimaryKey(primaryKey);
+	}
+
+	@Override
+	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
+		_guestbook.setPrimaryKeyObj(primaryKeyObj);
+	}
+
+	/**
+	 * Sets the user ID of this guestbook.
+	 *
+	 * @param userId the user ID of this guestbook
+	 */
+	@Override
+	public void setUserId(long userId) {
+		_guestbook.setUserId(userId);
+	}
+
+	/**
+	 * Sets the user name of this guestbook.
+	 *
+	 * @param userName the user name of this guestbook
+	 */
+	@Override
+	public void setUserName(String userName) {
+		_guestbook.setUserName(userName);
+	}
+
+	/**
+	 * Sets the user uuid of this guestbook.
+	 *
+	 * @param userUuid the user uuid of this guestbook
+	 */
+	@Override
+	public void setUserUuid(String userUuid) {
+		_guestbook.setUserUuid(userUuid);
+	}
+
+	/**
+	 * Sets the uuid of this guestbook.
+	 *
+	 * @param uuid the uuid of this guestbook
+	 */
+	@Override
+	public void setUuid(String uuid) {
+		_guestbook.setUuid(uuid);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.model.CacheModel<Guestbook>
+		toCacheModel() {
+
+		return _guestbook.toCacheModel();
+	}
+
+	@Override
+	public Guestbook toEscapedModel() {
+		return new GuestbookWrapper(_guestbook.toEscapedModel());
+	}
+
+	@Override
+	public String toString() {
+		return _guestbook.toString();
+	}
+
+	@Override
+	public Guestbook toUnescapedModel() {
+		return new GuestbookWrapper(_guestbook.toUnescapedModel());
+	}
+
+	@Override
+	public String toXmlString() {
+		return _guestbook.toXmlString();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof GuestbookWrapper)) {
+			return false;
+		}
+
+		GuestbookWrapper guestbookWrapper = (GuestbookWrapper)object;
+
+		if (Objects.equals(_guestbook, guestbookWrapper._guestbook)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public StagedModelType getStagedModelType() {
+		return _guestbook.getStagedModelType();
+	}
+
+	@Override
+	public Guestbook getWrappedModel() {
+		return _guestbook;
+	}
+
+	@Override
+	public boolean isEntityCacheEnabled() {
+		return _guestbook.isEntityCacheEnabled();
+	}
+
+	@Override
+	public boolean isFinderCacheEnabled() {
+		return _guestbook.isFinderCacheEnabled();
+	}
+
+	@Override
+	public void resetOriginalValues() {
+		_guestbook.resetOriginalValues();
+	}
+
+	private final Guestbook _guestbook;
+
+}
---
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/http/EntryServiceHttp.java
@@ -0,0 +1,360 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.docs.guestbook.service.http;
+
+import com.liferay.docs.guestbook.service.EntryServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.HttpPrincipal;
+import com.liferay.portal.kernel.service.http.TunnelUtil;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
+/**
+ * Provides the HTTP utility for the
+ * <code>EntryServiceUtil</code> service
+ * utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it requires an additional
+ * <code>HttpPrincipal</code> parameter.
+ *
+ * <p>
+ * The benefits of using the HTTP utility is that it is fast and allows for
+ * tunneling without the cost of serializing to text. The drawback is that it
+ * only works with Java.
+ * </p>
+ *
+ * <p>
+ * Set the property <b>tunnel.servlet.hosts.allowed</b> in portal.properties to
+ * configure security.
+ * </p>
+ *
+ * <p>
+ * The HTTP utility is only generated for remote services.
+ * </p>
+ *
+ * @author liferay
+ * @see EntryServiceSoap
+ * @generated
+ */
+public class EntryServiceHttp {
+
+	public static com.liferay.docs.guestbook.model.Entry addEntry(
+			HttpPrincipal httpPrincipal, long userId, long guestbookId,
+			String name, String email, String message,
+			com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "addEntry", _addEntryParameterTypes0);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, userId, guestbookId, name, email, message,
+				serviceContext);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.docs.guestbook.model.Entry)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static com.liferay.docs.guestbook.model.Entry updateEntry(
+			HttpPrincipal httpPrincipal, long userId, long guestbookId,
+			long entryId, String name, String email, String message,
+			com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			   com.liferay.portal.kernel.exception.SystemException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "updateEntry",
+				_updateEntryParameterTypes1);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, userId, guestbookId, entryId, name, email, message,
+				serviceContext);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.SystemException) {
+
+					throw (com.liferay.portal.kernel.exception.SystemException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.docs.guestbook.model.Entry)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static com.liferay.docs.guestbook.model.Entry deleteEntry(
+			HttpPrincipal httpPrincipal, long entryId,
+			com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "deleteEntry",
+				_deleteEntryParameterTypes2);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, entryId, serviceContext);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.docs.guestbook.model.Entry)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static java.util.List<com.liferay.docs.guestbook.model.Entry>
+		getEntries(
+			HttpPrincipal httpPrincipal, long groupId, long guestbookId) {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "getEntries",
+				_getEntriesParameterTypes3);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, groupId, guestbookId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (java.util.List<com.liferay.docs.guestbook.model.Entry>)
+				returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static java.util.List<com.liferay.docs.guestbook.model.Entry>
+			getEntries(
+				HttpPrincipal httpPrincipal, long groupId, long guestbookId,
+				int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "getEntries",
+				_getEntriesParameterTypes4);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, groupId, guestbookId, start, end);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.SystemException) {
+
+					throw (com.liferay.portal.kernel.exception.SystemException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (java.util.List<com.liferay.docs.guestbook.model.Entry>)
+				returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static java.util.List<com.liferay.docs.guestbook.model.Entry>
+		getEntries(
+			HttpPrincipal httpPrincipal, long groupId, long guestbookId,
+			int start, int end,
+			com.liferay.portal.kernel.util.OrderByComparator
+				<com.liferay.docs.guestbook.model.Entry> obc) {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "getEntries",
+				_getEntriesParameterTypes5);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, groupId, guestbookId, start, end, obc);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (java.util.List<com.liferay.docs.guestbook.model.Entry>)
+				returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static int getEntriesCount(
+		HttpPrincipal httpPrincipal, long groupId, long guestbookId) {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				EntryServiceUtil.class, "getEntriesCount",
+				_getEntriesCountParameterTypes6);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, groupId, guestbookId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return ((Integer)returnObj).intValue();
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(EntryServiceHttp.class);
+
+	private static final Class<?>[] _addEntryParameterTypes0 = new Class[] {
+		long.class, long.class, String.class, String.class, String.class,
+		com.liferay.portal.kernel.service.ServiceContext.class
+	};
+	private static final Class<?>[] _updateEntryParameterTypes1 = new Class[] {
+		long.class, long.class, long.class, String.class, String.class,
+		String.class, com.liferay.portal.kernel.service.ServiceContext.class
+	};
+	private static final Class<?>[] _deleteEntryParameterTypes2 = new Class[] {
+		long.class, com.liferay.portal.kernel.service.ServiceContext.class
+	};
+	private static final Class<?>[] _getEntriesParameterTypes3 = new Class[] {
+		long.class, long.class
+	};
+	private static final Class<?>[] _getEntriesParameterTypes4 = new Class[] {
+		long.class, long.class, int.class, int.class
+	};
+	private static final Class<?>[] _getEntriesParameterTypes5 = new Class[] {
+		long.class, long.class, int.class, int.class,
+		com.liferay.portal.kernel.util.OrderByComparator.class
+	};
+	private static final Class<?>[] _getEntriesCountParameterTypes6 =
+		new Class[] {long.class, long.class};
+
+}
